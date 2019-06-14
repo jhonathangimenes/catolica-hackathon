d667065289b5af92226c029ad36315469586920e
unknown <gimenesjhonathan@gmail.com>
2019-06-14 17:32:13 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+    <NavBar/>
+    <v-content>
+      <v-container fluid>
+        <transition name="list" mode="out-in">
+          <router-view></router-view>
+        </transition>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import NavBar from "@/components/navbar/NavBar.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavBar
+  },
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
+<style scoped>
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(-70px);
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s;
+}
+.list-move {
+  transition: all 0.5s;
+}
+.list-leave-active {
+  position: absolute;
+  width: calc(100% - 31px);
+}
+</style>
