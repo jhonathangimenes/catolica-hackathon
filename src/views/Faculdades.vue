71fa89dd1962d1a4e224de7aaba63f87ca5f2eb2
Jhonathan Mangueira Gimenes <gimenesjhonathan@gmail.com>
2019-06-15 05:07:24 -0300

    correcao dos layouts

--- src/views/Faculdades.vue
+++ src/views/Faculdades.vue
@@ -1,36 +1,46 @@
 <template>
-  <v-layout align-start>
-    <v-expansion-panel>
-      <v-expansion-panel-content v-for="faculdade in cursos[$route.params.id].faculdades" :key="faculdade.nome">
-        <template v-slot:header>
-          <div>
-            <v-avatar color="grey lighten-4" size="60">
-              <img :src="require('../assets/'+faculdade.logo)" alt="avatar">
-            </v-avatar>
-            <span class="subheading ml-2">{{ faculdade.nome }}</span>
-          </div>
-        </template>
-        <v-card>
-          <v-card-text>
-            <p>Mensalidade: {{ faculdade.mensalidade }}</p>
-            <p class="font-weight-bold color-red">{{ faculdade.desconto == 'sem desconto' ? 'Sem desconto' : 'Com desconto: ' + faculdade.desconto }}</p>
-            <p>{{ faculdade.endereco }}</p>
-            <v-layout justify-end>
-              <v-btn @click="$router.push(`/matricular/${$route.params.id}/${faculdade.id}`)" class="success">Matricule-se</v-btn>
-            </v-layout>
-          </v-card-text>
-        </v-card>
-      </v-expansion-panel-content>
-    </v-expansion-panel>
+  <v-layout justify-center align-start>
+    <v-flex md6>
+      <v-expansion-panel>
+        <v-expansion-panel-content
+          v-for="faculdade in cursos[$route.params.id].faculdades"
+          :key="faculdade.nome"
+        >
+          <template v-slot:header>
+            <div>
+              <v-avatar color="grey lighten-4" size="60">
+                <img :src="require('../assets/'+faculdade.logo)" alt="avatar">
+              </v-avatar>
+              <span class="subheading ml-2">{{ faculdade.nome }}</span>
+            </div>
+          </template>
+          <v-card>
+            <v-card-text>
+              <p>Mensalidade: {{ faculdade.mensalidade }}</p>
+              <p
+                class="font-weight-bold color-red"
+              >{{ faculdade.desconto == 'sem desconto' ? 'Sem desconto' : 'Com desconto: ' + faculdade.desconto }}</p>
+              <p>{{ faculdade.endereco }}</p>
+              <v-layout justify-end>
+                <v-btn
+                  @click="$router.push(`/matricular/${$route.params.id}/${faculdade.id}`)"
+                  class="success"
+                >Matricule-se</v-btn>
+              </v-layout>
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState } from "vuex";
 
 export default {
   computed: {
-    ...mapState(['cursos'])
+    ...mapState(["cursos"])
   }
 };
 </script>
--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,41 +1,48 @@
 <template>
-  <v-layout justify-start>
-    <form class="w-100">
-      <v-text-field label="Name" value="Pedro Gomes Silva" required></v-text-field>
-      <v-text-field label="E-mail" value="pedro.silva@gmail.com" required></v-text-field>
-      <v-text-field disabled label="Curso" :value="cursosDesc[$route.params.cursoId].nome" required></v-text-field>
-      <v-text-field label="Data Nascimento" value="23/05/2001" required></v-text-field>
-      <v-text-field disabled label="Valor mensalidade" :value="valorFormatado" required></v-text-field>
-      <v-checkbox
-        value="1"
-        label="Aceitar os termos"
-        data-vv-name="checkbox"
-        type="checkbox"
-        required
-      ></v-checkbox>
-      <div class="text-xs-center">
-        <v-dialog v-model="dialog">
-          <template v-slot:activator="{ on }">
-            <v-btn class="btn-termos" flat small dark v-on="on">Ler termos</v-btn>
-          </template>
+  <v-layout justify-center>
+    <v-flex md6>
+      <form class="w-100">
+        <v-text-field label="Name" value="Pedro Gomes Silva" required></v-text-field>
+        <v-text-field label="E-mail" value="pedro.silva@gmail.com" required></v-text-field>
+        <v-text-field
+          disabled
+          label="Curso"
+          :value="cursosDesc[$route.params.cursoId].nome"
+          required
+        ></v-text-field>
+        <v-text-field label="Data Nascimento" value="23/05/2001" required></v-text-field>
+        <v-text-field disabled label="Valor mensalidade" :value="valorFormatado" required></v-text-field>
+        <v-checkbox
+          value="1"
+          label="Aceitar os termos"
+          data-vv-name="checkbox"
+          type="checkbox"
+          required
+        ></v-checkbox>
+        <div class="text-xs-center">
+          <v-dialog v-model="dialog">
+            <template v-slot:activator="{ on }">
+              <v-btn class="btn-termos" flat small dark v-on="on">Ler termos</v-btn>
+            </template>
 
-          <v-card>
-            <v-card-title class="headline grey lighten-2" primary-title>Termos</v-card-title>
+            <v-card>
+              <v-card-title class="headline grey lighten-2" primary-title>Termos</v-card-title>
 
-            <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</v-card-text>
-          </v-card>
-        </v-dialog>
-      </div>
-      <v-layout justify-end>
-        <v-btn class="error">Cancelar</v-btn>
-        <v-btn @click="matricular()" class="success">Matricular</v-btn>
-      </v-layout>
-    </form>
+              <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</v-card-text>
+            </v-card>
+          </v-dialog>
+        </div>
+        <v-layout justify-end>
+          <v-btn class="error">Cancelar</v-btn>
+          <v-btn @click="matricular()" class="success">Matricular</v-btn>
+        </v-layout>
+      </form>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
 export default {
   data() {
     return {
@@ -43,13 +50,19 @@ export default {
     };
   },
   computed: {
-    ...mapState(['cursos', 'cursosDesc']),
+    ...mapState(["cursos", "cursosDesc"]),
     valorFormatado() {
-      let valor = ''
-      this.cursos[this.$route.params.cursoId].faculdades[this.$route.params.faculdadeId].desconto == 'sem desconto'
-      ? valor = this.cursos[this.$route.params.cursoId].faculdades[this.$route.params.faculdadeId].mensalidade
-      : valor = this.cursos[this.$route.params.cursoId].faculdades[this.$route.params.faculdadeId].desconto
-      return valor
+      let valor = "";
+      this.cursos[this.$route.params.cursoId].faculdades[
+        this.$route.params.faculdadeId
+      ].desconto == "sem desconto"
+        ? (valor = this.cursos[this.$route.params.cursoId].faculdades[
+            this.$route.params.faculdadeId
+          ].mensalidade)
+        : (valor = this.cursos[this.$route.params.cursoId].faculdades[
+            this.$route.params.faculdadeId
+          ].desconto);
+      return valor;
     }
   },
   methods: {
@@ -66,11 +79,11 @@ export default {
   width: 100%;
 }
 .btn-termos {
-    color: blue!important;
-    margin: -10px 0px 20px 0px!important;
-    padding: 0px!important;
+  color: blue !important;
+  margin: -10px 0px 20px 0px !important;
+  padding: 0px !important;
 }
 .text-xs-center {
-    text-align: left!important;
+  text-align: left !important;
 }
 </style>
