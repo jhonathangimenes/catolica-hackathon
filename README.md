45c7d62773fef271617a5926abb156ea3552aa2b
andersonAncilon <dan100anderson@gmail.com>
2019-06-15 06:35:40 -0300

    Criação do painel de usuário

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # catolica-hackathon
+
+## Introdução
+
+```
+A aplicação surge com o intuito de auxiliar alunos do Ensino Médio a escolher a sua futura carreira profissional. Isso é feito através de sugestões de cursos baseadas na vocação do aluno, essa vocação é definida através de suas notas, em seu histórico escolar.
+```
--- src/views/Painel.vue
+++ src/views/Painel.vue
@@ -1,6 +1,39 @@
 <template>
-  <v-layout column wrap align-center>
+  <v-container grid-list-md>
     <h2>Painel</h2>
-    <v-flex md4></v-flex>
-  </v-layout>
+    <v-layout row wrap>
+      <v-flex md6 v-for="materia in materias" :key="materia.id">
+        <v-card color="blue-grey darken-2" class="white--text">
+          <v-card-title primary-title>
+            <div>
+              <div class="headline">{{materia.nome}}</div>
+              <v-progress-circular
+                :rotate="360"
+                :size="65"
+                :width="15"
+                :value="materia.nota * 10"
+                color="teal"
+              >{{materia.nota * 10}}%</v-progress-circular>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    materias: [
+      { id: 0, nome: "Português", nota: 8.0 },
+      { id: 1, nome: "História", nota: 7.5 },
+      { id: 2, nome: "Geografia", nota: 7.0 },
+      { id: 3, nome: "Matemática", nota: 9.5 },
+      { id: 4, nome: "Física", nota: 9.0 },
+      { id: 5, nome: "Filosofia", nota: 9.8 }
+    ]
+  })
+};
+</script>
+
