60e9ce281dd44ad5660673e77a43d373a09adb73
andersonAncilon <dan100anderson@gmail.com>
2019-06-15 01:49:20 -0300

    Corrigindo conflitos

--- src/router.js
+++ src/router.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
-import Faculdades from './views/Faculdades.vue';
+import Faculdades from './views/Faculdades.vue'
 import Cursos from './views/Cursos.vue'
-=======
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home.vue';
-import Login from './views/Login.vue';
-import Faculdades from './views/Faculdades.vue';
-import Cursos from './views/Cursos.vue'
->>>>>>> 8ae7671c494303e7bde61015df78a1f4574a8bdf
 
 Vue.use(Router)
 
@@ -30,14 +21,13 @@ export default new Router({
       component: Login
     },
     {
-<<<<<<< HEAD
       path: '/:area',
       component: Cursos
-=======
+    },
+    {
       path: '/faculdades',
       name: 'faculdades',
       component: Faculdades
->>>>>>> 8ae7671c494303e7bde61015df78a1f4574a8bdf
     }
   ]
 })
